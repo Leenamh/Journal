92ce81053a8b975a61c4b4f299a1be8ea369a883
leena <LeenaAlmusharraf@icloud.com>
2025-10-19 14:47:47 +0300

    Initial Commit

---
+++ JournalApp/JournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalAppApp.swift
+//  JournalApp
+//
+//  Created by leena almusharraf on 19/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct JournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
